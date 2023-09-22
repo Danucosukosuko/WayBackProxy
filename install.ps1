b03a197a5aa6f0ce2c1ea91661481f095817519a
Danucosuko suko <71569318+Danucosukosuko@users.noreply.github.com>
2023-09-22 14:58:29 +0200

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,31 @@
+# Pantalla de inicio
+Clear-Host
+Write-Host "╔════════════════════════╗"
+Write-Host "║                        ║"
+Write-Host "║ Instalar WayBackProxy  ║"
+Write-Host "║                        ║"
+Write-Host "╚════════════════════════╝"
+Write-Host "Presiona 'I' para continuar..."
+$key = [System.Console]::ReadKey().Key
+
+if ($key -ne 'I') {
+    Write-Host "La tecla presionada no es 'I'. Saliendo..."
+    exit
+}
+
+# Obtiene la ruta actual
+$scriptPath = $PSScriptRoot
+
+# Registra el servicio
+New-Service -Name "ProxyService" -BinaryPathName "$scriptPath\proxy_server.py" -DisplayName "Proxy Service" -Description "Proxy Server Service"
+
+# Inicia el servicio
+Start-Service -Name "ProxyService"
+
+# Limpia la pantalla y muestra el mensaje de instalación correcta
+Clear-Host
+Write-Host "╔══════════════════════════╗"
+Write-Host "║                          ║"
+Write-Host "║ Instalado correctamente  ║"
+Write-Host "║                          ║"
+Write-Host "╚══════════════════════════╝"
